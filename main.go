544189ca418bdabd05ce1d89d51bed9e5879acb9
davisssamuel <davissamuelg1@gmail.com>
2023-12-01 16:18:42 +0000

    tweaks to index.css and main.go print statements

--- main.go
+++ main.go
@@ -9,13 +9,13 @@ import (
 func main() {
 
 	http.Handle("/static/",
-    http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+		http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    tmpl := template.Must(template.ParseFiles("./templates/index.html"))
-    tmpl.Execute(w, nil)
-  })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("./templates/index.html"))
+		tmpl.Execute(w, nil)
+	})
 
-  log.Println("Site running on port 3000...")
-  log.Fatal(http.ListenAndServe(":3000", nil))
+	log.Println("site running on port 3000...")
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }

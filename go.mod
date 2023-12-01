38eb99cf11faf47cee44b123883614a0ff3f0564
davisssamuel <davissamuelg1@gmail.com>
2023-12-01 10:01:05 -0500

    switched from JavaScript to Go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module davisssamuel.net/main
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.Handle("/static/",
+    http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    tmpl := template.Must(template.ParseFiles("./templates/index.html"))
+    tmpl.Execute(w, nil)
+  })
+
+  log.Println("Site running on port 3000...")
+  log.Fatal(http.ListenAndServe(":3000", nil))
+}
